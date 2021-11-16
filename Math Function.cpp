d38d424d8cf6ac4fd34bc796b19e44f00f4c3ebc
SaloniSinghal16 <94491955+SaloniSinghal16@users.noreply.github.com>
2021-11-16 17:20:42 -0500

    Add files via upload

---
+++ Math Function.cpp
@@ -0,0 +1,22 @@
+//
+//  Math Function_Sum.cpp
+//  Math Functions_1
+//
+//  Created by Saloni Singhal on 9/23/21.
+//
+
+#include <iostream>
+int main() {
+    int number1{0};
+    int number2{0};
+    int remainder{0};
+    
+    std::cout<<"Enter first number: ";
+    std::cin>> number1;
+    
+    std::cout<<"Enter second number: ";
+    std::cin>> number2;
+    
+    remainder=number1%number2;
+    std::cout<<"The remainder of two numbers is: "<< remainder <<std::endl;
+}
